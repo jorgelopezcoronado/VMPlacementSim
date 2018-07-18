bff57b91a1005fdb50e561ae191fc96c00a832bc
Jorge López <jorge@Kira.local>
2018-07-18 13:09:26 +0300

    Added package structure

--- VMPlacementAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package vmplacementsim;
-
-public interface VMPlacementAlgorithm
-{
-	public String getName();
-	//we encourage anybody implementing this method to first check if pm has the correct conf, i.e., if(pm.correctConfiguration(pc)) as the first thing this method does
-	public void place(PlacementConfiguration pc, Request r, PlacementModule pm);
-}
